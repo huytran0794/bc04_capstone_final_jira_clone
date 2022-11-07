55a1bbbe1e42c77c1cd372fe2ab04ccf9b68fc5e
Huy Tran <huytran21007@gmail.com>
2022-11-08 00:22:19 +0700

    Show basic task card info if have data

--- src/Pages/ProjectPage/ProjectDetail/DetailIssueBoard.tsx
+++ src/Pages/ProjectPage/ProjectDetail/DetailIssueBoard.tsx
@@ -3,12 +3,54 @@ import React from 'react'
 import SimpleMemberAvatar from '../../../core/Components/Avatar/SimpleMemberAvatar';
 import { IProjectDetail } from '../../../core/models/Project/Project.interface';
 import { BsFillBookmarkFill } from "react-icons/bs"
-import { ITaskDetailList } from '../../../core/models/Task/Task.Interface';
+import { ITask, ITaskDetailList } from '../../../core/models/Task/Task.Interface';
+import clsx from 'clsx';
 const DetailIssueBoard = ({ projectDetail }: Partial<IProjectDetail>) => {
+    const renderProjectCard = (lstTaskDeTail: ITask[]) => {
+        if (lstTaskDeTail.length) {
+            return lstTaskDeTail.map((task: ITask, idx: number) => {
+                return (
+                    <div className={clsx(
+                        "card cursor-pointer select-none",
+                        "rounded-[3px] bg-white shadow-md",
+                        "text-[#172B4D] hover:bg-[#F4F5F7] hover:text-[#172B4D]",
+                        "transition-all duration-700"
+                    )}>
+                        <div className="card-content p-3">
+                            <div className="card__title text-sm mb-9">
+                                <h6>{task.taskName}</h6>
+                            </div>
+                            <div className="card__info">
+                                <div className="wrapper flex items-center justify-between">
+                                    <div className="card__info-col--left flex">
+                                        <div className="type">
+                                            {task.taskTypeDetail.taskType}
+                                        </div>
+                                        <div className="priority">
+                                            {task.priorityTask.priority}
+                                        </div>
+                                    </div>
+                                    <div className="card__info-col--left">
+                                        <div className="member">
+                                            <Avatar.Group maxCount={2} size={35}>
+                                                <SimpleMemberAvatar members={task.assigness} />
+                                            </Avatar.Group>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    }
     const projectBoard = (
         <div className="wrapper project__board flex items-center gap-5">
             {
                 projectDetail?.lstTask.map((taskDetailList: ITaskDetailList, idx: number) => {
+                    console.log(taskDetailList);
+
                     return (
                         <div className="col w-1/4 shadow-md" key={idx}>
                             <div className="content-wrapper flex-1 px-2 bg-[#F4F5F7] shadow-md">
@@ -24,54 +66,7 @@ const DetailIssueBoard = ({ projectDetail }: Partial<IProjectDetail>) => {
                                     </div>
                                     <div className="card__list-container min-h-[80px] pb-3 max-w-full truncate">
                                         <div className="wrapper px-2 pt-2 flex flex-col gap-4">
-                                            <div className="card cursor-pointer select-none rounded-[3px] bg-white shadow-md text-[#172B4D] hover:bg-[#F4F5F7] hover:text-[#172B4D] transition-all duration-700">
-                                                <div className="card-content p-3">
-                                                    <div className="card__title text-sm mb-9">
-                                                        <h6>This is an issue of type: Task.</h6>
-                                                    </div>
-                                                    <div className="card__info">
-                                                        <div className="wrapper flex items-center justify-between">
-                                                            <div className="card__info-col--left flex">
-                                                                <div className="type">
-                                                                    <BsFillBookmarkFill />
-                                                                </div>
-                                                                <div className="priority"></div>
-                                                            </div>
-                                                            <div className="card__info-col--left">
-                                                                <div className="member">
-                                                                    <Avatar.Group maxCount={2} size={35}>
-                                                                        <SimpleMemberAvatar members={projectDetail?.members} />
-                                                                    </Avatar.Group>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="card cursor-pointer select-none rounded-[3px] bg-white shadow-md text-[#172B4D] hover:bg-[#F4F5F7] hover:text-[#172B4D] transition-all duration-700">
-                                                <div className="card-content p-3">
-                                                    <div className="card__title text-sm mb-9">
-                                                        You can track how many hours were spent working on an issue, and how many hours remain.
-                                                    </div>
-                                                    <div className="card__info">
-                                                        <div className="wrapper flex items-center justify-between">
-                                                            <div className="card__info-col--left flex">
-                                                                <div className="type">
-                                                                    <BsFillBookmarkFill />
-                                                                </div>
-                                                                <div className="priority"></div>
-                                                            </div>
-                                                            <div className="card__info-col--left">
-                                                                <div className="member">
-                                                                    <Avatar.Group maxCount={2} size={35}>
-                                                                        <SimpleMemberAvatar members={projectDetail?.members} />
-                                                                    </Avatar.Group>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            {renderProjectCard(taskDetailList.lstTaskDeTail)}
                                         </div>
                                     </div>
                                 </div>
--- src/core/models/Task/Task.Interface.ts
+++ src/core/models/Task/Task.Interface.ts
@@ -1,8 +1,18 @@
+import { User } from "../User/User.interface";
+
 export interface ITaskDetailList {
   statusId: number;
   statusName: string;
   alias: string;
   lstTaskDeTail: ITask[];
+}
+
+export interface ITaskDetail {
+  taskId: number;
+  taskName: string;
+  alias: string;
+  description: string;
+  statusId: string;
 }
 export interface ITask {
   listUserAsign: [];
@@ -13,7 +23,38 @@ export interface ITask {
   originalEstimate: number;
   timeTrackingSpent: number;
   timeTrackingRemaining: number;
-  projectId: number;
   typeId: number;
   priorityId: number;
+  projectId: number;
+  lstComment: ITaskComment[];
+  priorityTask: Partial<ITaskPriority>;
+  taskTypeDetail: ITaskType;
+  assigness: Partial<User>[];
+}
+
+export interface ITaskPriority {
+  priorityId: number;
+  priority: string;
+  description: string;
+  alias: string;
+}
+export interface ITaskType {
+  id: number;
+  taskType: "bug" | "new task";
+}
+
+export interface ITaskStatus {
+  statusId: string;
+  statusName: string;
+  alias: string;
+}
+
+
+export interface ITaskComment {
+  user: Partial<User>;
+  id: number;
+  userId: number;
+  taskId: number;
+  contentComment: string;
+  alias: string;
 }
